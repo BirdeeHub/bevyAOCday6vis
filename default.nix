9b5ef7d6918c3fc89681c076892670904821d763
BirdeeHub <birdee@localhost>
2024-12-12 05:53:47 -0800

    This changes nothing, don't look
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   .cargo/config.toml
    	modified:   default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
 { APPNAME
+, lib
 , makeRustPlatform
 , fenix
 , writeShellScriptBin
 , pkg-config
 , alsa-lib
-, libudev-zero
+, udev
+, vulkan-loader
+, libxkbcommon
 , libX11
 , libxcb
+, pkgs
 , ...
 }: let
 APPDRV = (makeRustPlatform fenix.packages.x86_64-linux.default).buildRustPackage {
@@ -14,7 +18,26 @@ APPDRV = (makeRustPlatform fenix.packages.x86_64-linux.default).buildRustPackage
   version = "0.0.0";
   src = ./.;
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ libX11 libxcb alsa-lib libudev-zero ];
+  buildInputs = with pkgs; [
+    alsa-lib
+    udev
+    vulkan-loader
+    llvmPackages.bintools
+    clang
+    pkg-config
+    libX11
+    libxcb
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libxkbfile
+    libxkbcommon
+    vulkan-tools
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+  ];
 
   cargoLock = {
     lockFileContents = builtins.readFile ./Cargo.lock;
@@ -24,5 +47,6 @@ APPDRV = (makeRustPlatform fenix.packages.x86_64-linux.default).buildRustPackage
 in
 writeShellScriptBin APPNAME ''
   export AOC_INPUT=''${1:-./${APPNAME}/input};
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath [ alsa-lib udev vulkan-loader libxkbcommon]}"
   exec ${APPDRV}/bin/${APPNAME}
 ''
