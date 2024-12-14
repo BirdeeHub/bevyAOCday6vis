dbc0c2999bf007d7d1195bbde2f34cf4f3253674
BirdeeHub <birdee@localhost>
2024-12-14 05:45:24 -0800

    This was the most stupid bug in the world, fixed in the smartest way ever
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   shell.nix
    	modified:   src/examples/input4.txt

--- shell.nix
+++ shell.nix
@@ -35,6 +35,8 @@
         hash = "sha256-1AN2E9t/lZhbXdVznhTcniy+7ZzlaEp/gwLEAucs6EA=";
         cargoHash = "sha256-DbwAh8RJtW38LJp+J9Ht8fAROK9OabaJ85D9C/Vkve4=";
       })
+      binaryen
+      wasm-tools
       wasm-pack
       cargo-edit
       alsa-lib
